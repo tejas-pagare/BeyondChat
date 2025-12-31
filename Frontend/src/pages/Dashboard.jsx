3db761a3aab74b1a0a5768e13dfb9546d4b2caa9
TEJAS PAGARE <tejaspagare@TEJASs-MacBook-Air.local>
2025-12-31 21:17:21 +0530

    refactor: update ArticleCard refresh handler to use fetchArticles

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -111,7 +111,7 @@ const Dashboard = () => {
             ) : (
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {articles.map((article) => (
-                        <ArticleCard key={article._id} article={article} onRefresh={handleRefresh} />
+                        <ArticleCard key={article._id} article={article} onRefresh={fetchArticles} />
                     ))}
                 </div>
             )}
